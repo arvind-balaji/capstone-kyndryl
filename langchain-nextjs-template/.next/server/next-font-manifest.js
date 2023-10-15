ddc86e7d36453a4d7893d3d6f1d5cd942492c29f
fool1280 <ng.huyanh.gd@gmail.com>
2023-10-15 16:00:58 -0500

    delete node_modules

--- langchain-nextjs-template/.next/server/next-font-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__NEXT_FONT_MANIFEST="{\"pages\":{},\"app\":{\"/Users/anh/Data/csce482/capstone-kyndryl/langchain-nextjs-template/app/layout\":[\"static/media/6a44acd4f1e705fc-s.p.woff2\"]},\"appUsingSizeAdjust\":true,\"pagesUsingSizeAdjust\":false}"
