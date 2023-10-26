4edad4f1f86a1f4b4f3a2bcd98bb2c7097547fdd
Arvind Balaji <arvindb@tamu.edu>
2023-10-26 18:17:06 -0500

    Add route handler

---
+++ main/app/api/retrieval/file_ingest/route.ts
@@ -0,0 +1,40 @@
+import { NextRequest, NextResponse } from "next/server";
+import { RecursiveCharacterTextSplitter } from "langchain/text_splitter";
+
+import { createClient } from "@supabase/supabase-js";
+import { SupabaseVectorStore } from "langchain/vectorstores/supabase";
+import { OpenAIEmbeddings } from "langchain/embeddings/openai";
+import { PDFLoader } from "langchain/document_loaders/fs/pdf";
+
+// import {writeFile} from 'fs'
+
+export const runtime = "edge";
+
+// Before running, follow set-up instructions at
+// https://js.langchain.com/docs/modules/indexes/vector_stores/integrations/supabase
+
+/**
+ * This handler takes input text, splits it into chunks, and embeds those chunks
+ * into a vector store for later retrieval. See the following docs for more information:
+ *
+ * https://js.langchain.com/docs/modules/data_connection/document_transformers/text_splitters/recursive_text_splitter
+ * https://js.langchain.com/docs/modules/data_connection/vectorstores/integrations/supabase
+ */
+export async function POST(req: NextRequest) {
+//   const body = await req.json();
+//   const text = body.text;
+
+
+  try {
+    const data = await req.formData();
+    const file: File | null = data.get('file') as unknown as File
+    const loader = new PDFLoader(file);
+    const docs = await loader.load();
+    
+    console.log(docs);
+    // await writeFile("~/Documents/foo.pdf", Buffer.from(await file.arrayBuffer()));
+    return NextResponse.json({ ok: true }, { status: 200 });
+  } catch (e: any) {
+    return NextResponse.json({ error: e.message }, { status: 500 });
+  }
+}
