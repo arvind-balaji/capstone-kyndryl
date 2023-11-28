6fa56ca474c229bdd4e183e6ec2d45ea89240c20
Arvind Balaji <arvindb@tamu.edu>
2023-11-27 20:28:46 -0600

    Update Dockerfile

--- main/app/documents/page.tsx
+++ main/app/documents/page.tsx
@@ -8,7 +8,7 @@ import { GetServerSideProps } from "next";
 import { createClient } from "@supabase/supabase-js";
 
 
-export const getFiles = cache(async () => {
+const getFiles = cache(async () => {
   const client = createClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_PRIVATE_KEY!,
